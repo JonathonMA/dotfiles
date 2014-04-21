14e0aa2bbd48f9477d96b93cee7aea057bec949a
Jonathon M. Abbott <jma@dandaraga.net>
2014-04-21 23:35:37 +1000

    wip

--- shell/config/misc.sh
+++ shell/config/misc.sh
@@ -24,12 +24,3 @@ shopt -s checkwinsize
 if [ -z "${debian_chroot:-}" ] && [ -r /etc/debian_chroot ]; then
     debian_chroot=$(cat /etc/debian_chroot)
 fi
-
-# If this is an xterm set the title to user@host:dir
-case "$TERM" in
-xterm*|rxvt*)
-    PS1="\[\e]0;${debian_chroot:+($debian_chroot)}\u@\h: \w\a\]$PS1"
-    ;;
-*)
-    ;;
-esac
