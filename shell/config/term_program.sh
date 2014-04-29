f600c09d644bbe03e70431bac07e1d2423c910c3
Jonathon Abbott <jonathona@everydayhero.com.au>
2014-04-30 09:27:21 +1000

    Fix COLORTERM check

--- shell/config/term_program.sh
+++ shell/config/term_program.sh
@@ -1,4 +1,4 @@
 # Set TERM_PROGRAM (because then vim solarized will use italics!)
-if [ $COLORTERM = "gnome-terminal" ]; then
+if [ x$COLORTERM = x"gnome-terminal" ]; then
   export TERM_PROGRAM="gnome-terminal"
 fi
