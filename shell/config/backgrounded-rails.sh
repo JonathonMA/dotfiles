08ca7d696e4f5cf764911db2b6fd451453695b9f
Jonathon Abbott <jonathona@everydayhero.com.au>
2014-04-30 15:36:07 +1000

    Add Backgrounded rails helpers

---
+++ shell/config/backgrounded-rails.sh
@@ -0,0 +1,17 @@
+# Backgrounded rails development
+# Run these three in a backgrounded screen/tmux
+alias cgit='watch -n 90 git fetch'
+alias smartman='rerun -d .git -p CURRENT_BRANCH foreman start'
+alias smartzeus='TERM=xterm rerun -d .git -p CURRENT_BRANCH zeus start'
+
+# Use this to manually restart the backgrounded workers
+function restartforeman ()
+{
+  if [ -f ".git/CURRENT_BRANCH" ]; then
+    touch .git/CURRENT_BRANCH
+  else
+    echo "can't find CURRENT_BRANCH (probably not in git root)"
+  fi
+}
+
+alias rf=restartforeman
