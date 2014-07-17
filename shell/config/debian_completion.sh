f23fd682fd7c6c492738045f5576016caadb7517
Jonathon M. Abbott <jma@dandaraga.net>
2014-07-18 07:30:45 +1000

    Re-add missing __gitdir primitive

--- shell/config/debian_completion.sh
+++ shell/config/debian_completion.sh
@@ -7,4 +7,24 @@ if ! shopt -oq posix; then
   elif [ -f /etc/bash_completion ]; then
     . /etc/bash_completion
   fi
+
+  __gitdir ()
+  {
+    if [ -z "${1-}" ]; then
+      if [ -n "${__git_dir-}" ]; then
+        echo "$__git_dir"
+      elif [ -n "${GIT_DIR-}" ]; then
+        test -d "${GIT_DIR-}" || return 1
+        echo "$GIT_DIR"
+      elif [ -d .git ]; then
+        echo .git
+      else
+        git rev-parse --git-dir 2>/dev/null
+      fi
+    elif [ -d "$1/.git" ]; then
+      echo "$1/.git"
+    else
+      echo "$1"
+    fi
+  }
 fi
