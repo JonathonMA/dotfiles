57bf6bbd9edf33c17c3290e4ec77765dee8ba9e9
Jonathon M. Abbott <jma@dandaraga.net>
2014-07-28 23:19:42 +1000

    Update PATH
    - */sbin
    - /usr/games

--- shell/path.sh
+++ shell/path.sh
@@ -11,9 +11,12 @@ function append_path() {
 
 PATH=$(getconf PATH)
 
+prepend_path '/usr/sbin'
+prepend_path '/sbin'
 prepend_path '/usr/local/bin'
 prepend_path '/usr/local/sbin'
 append_path '/opt/X11/bin'
+append_path '/usr/games'
 
 unset prepend_path
 unset append_path
