cd4a146b79be14fa871ca34ff17ac08baff1fe51
Jonathon M. Abbott <jma@dandaraga.net>
2016-06-21 21:00:10 +1000

    [sh] Prompt: Use red for dirty git

--- shell/config/prompt.sh
+++ shell/config/prompt.sh
@@ -7,4 +7,4 @@ function __git_is_dirty() { git status --porcelain | grep -q .; }
 function __git_dirty() { __git_in_repo && __git_is_dirty && echo "$1"; }
 function __git_clean() { __git_in_repo && (__git_is_dirty || echo "$1"); }
 
-export PS1='$(__git_ps1 "\[\e[37m\](\[\e[0m\]")$(__git_dirty "\[\e[35m\]✗\[\e[0m\]")$(__git_clean "\[\e[32m\]✓\[\e[0m\]")$(__git_ps1 " \[\e[34m\]%s\[\e[0;37m\])\[\e[0m\] ")\[\e[36m\]\w\[\e[0;33m\]\$\[\e[0m\] '
+export PS1='$(__git_ps1 "\[\e[37m\](\[\e[0m\]")$(__git_dirty "\[\e[31m\]✗\[\e[0m\]")$(__git_clean "\[\e[32m\]✓\[\e[0m\]")$(__git_ps1 " \[\e[34m\]%s\[\e[0;37m\])\[\e[0m\] ")\[\e[36m\]\w\[\e[0;33m\]\$\[\e[0m\] '
