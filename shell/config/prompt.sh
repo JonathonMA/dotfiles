97f518ff67be22cf787c5acecc346c0e97bf9a34
Jonathon M. Abbott <jonathona@everydayhero.com.au>
2016-08-12 10:22:48 +1000

    sh: Add background jobs indicator to prompt

--- shell/config/prompt.sh
+++ shell/config/prompt.sh
@@ -6,5 +6,6 @@ function __git_in_repo() { __gitdir > /dev/null; }
 function __git_is_dirty() { git status --porcelain | grep -q .; }
 function __git_dirty() { __git_in_repo && __git_is_dirty && echo "$1"; }
 function __git_clean() { __git_in_repo && (__git_is_dirty || echo "$1"); }
+function __jobs_any() { jobs | grep -q . && echo "$1"; }
 
-export PS1='$(__git_ps1 "\[\e[37m\](\[\e[0m\]")$(__git_dirty "\[\e[31m\]✗\[\e[0m\]")$(__git_clean "\[\e[32m\]✓\[\e[0m\]")$(__git_ps1 " \[\e[34m\]%s\[\e[0;37m\])\[\e[0m\] ")\[\e[36m\]\w\[\e[0;33m\]\$\[\e[0m\] '
+export PS1='$(__git_ps1 "\[\e[37m\](\[\e[0m\]")$(__git_dirty "\[\e[31m\]✗\[\e[0m\]")$(__git_clean "\[\e[32m\]✓\[\e[0m\]")$(__git_ps1 " \[\e[34m\]%s\[\e[0;37m\])\[\e[0m\] ")$(__jobs_any "\[\e[33m\]ꕚ \[\e[0m\]")\[\e[36m\]\w\[\e[0;33m\]\$\[\e[0m\] '
