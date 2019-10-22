f77889cb5b7d8cc0ae54a032a35ad9bd7125fbc0
Jonathon M. Abbott <jon.abbott@envisionapp.com>
2019-10-22 13:17:18 +1000

    xdg: Configure psql and node history

--- shell/config/xdg.sh
+++ shell/config/xdg.sh
@@ -1,2 +1,4 @@
 export HISTFILE="$HOME/.cache/bash_history"
 export LESSHISTFILE="$HOME/.cache/less_history"
+export PSQL_HISTORY="$HOME/.cache/psql_history"
+export NODE_REPL_HISTORY="$HOME/.cache/node_repl_history"
