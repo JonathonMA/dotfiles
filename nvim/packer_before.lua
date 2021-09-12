8fea1f2ba8b47d7265dc3e89cf6f910c834f13f9
Jonathon M. Abbott <jma@dandaraga.net>
2021-09-12 21:25:03 +1000

    nvim: Add packer

---
+++ nvim/packer_before.lua
@@ -0,0 +1,10 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+-- Only required if you have packer configured as `opt`
+-- vim.cmd [[packadd packer.nvim]]
+-- Only if your version of Neovim doesn't have https://github.com/neovim/neovim/pull/12632 merged
+-- vim._update_package_paths()
+
+return require('packer').startup(function()
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
--- shell/aliases/vim.sh
+++ shell/aliases/vim.sh
@@ -1,3 +1,4 @@
 alias vim=nvim
 alias vplug='vim +PlugInstall +qall'
 alias vupdate='vim +PlugUpdate +qall'
+alias pupdate="nvim +:PackerCompile -c 'autocmd User PackerComplete quitall' -c 'PackerSync'"
