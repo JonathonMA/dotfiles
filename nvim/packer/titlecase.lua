923e0a0bc8038712152f2e5747522740b4ed9504
Jonathon M. Abbott <jma@dandaraga.net>
2022-10-28 16:19:52 +1000

    nvim: Convert titlecase to packer

---
+++ nvim/packer/titlecase.lua
@@ -0,0 +1,11 @@
+use {
+  'christoomey/vim-titlecase',
+  config = function()
+    vim.cmd [[
+      nmap <leader>gz  <Plug>Titlecase
+      vmap <leader>gz  <Plug>Titlecase
+      nmap <leader>gzz <Plug>TitlecaseLine
+    ]]
+  end,
+}
+
--- nvim/plugins/titlecase.vim
+++
@@ -1,5 +0,0 @@
-Plug 'christoomey/vim-titlecase'
-
-nmap <leader>gz  <Plug>Titlecase
-vmap <leader>gz  <Plug>Titlecase
-nmap <leader>gzz <Plug>TitlecaseLine
