3844475cfd528f458f99eaf5ee4ff0ca8277d9c0
Jonathon M. Abbott <jma@dandaraga.net>
2022-10-28 16:19:48 +1000

    nvim: Convert plug.vim to packer.lua

---
+++ nvim/packer.lua
@@ -0,0 +1,5 @@
+-- Sensible defaults
+use 'tpope/vim-sensible'
+
+-- Sensible paste behaviour
+use 'ConradIrwin/vim-bracketed-paste'
--- nvim/plug.vim
+++
@@ -1,5 +0,0 @@
-" Sensible defaults
-Plug 'tpope/vim-sensible'
-
-" Sensible paste behaviour
-Plug 'ConradIrwin/vim-bracketed-paste'
