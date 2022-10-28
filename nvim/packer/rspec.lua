a2b481ba2e0b79b5ed91634e362ef84b31a69aee
Jonathon M. Abbott <jma@dandaraga.net>
2022-10-28 16:19:51 +1000

    nvim: Convert rspec to packer

---
+++ nvim/packer/rspec.lua
@@ -0,0 +1,11 @@
+use {
+  'thoughtbot/vim-rspec',
+  config = function()
+    vim.cmd [[
+      map <Leader>rt :call RunCurrentSpecFile()<CR>
+      map <Leader>rs :call RunNearestSpec()<CR>
+      map <Leader>rl :call RunLastSpec()<CR>
+      map <Leader>ra :call RunAllSpecs()<CR>
+    ]]
+  end,
+}
--- nvim/plugins/rspec.vim
+++
@@ -1,6 +0,0 @@
-Plug 'thoughtbot/vim-rspec'
-
-map <Leader>rt :call RunCurrentSpecFile()<CR>
-map <Leader>rs :call RunNearestSpec()<CR>
-map <Leader>rl :call RunLastSpec()<CR>
-map <Leader>ra :call RunAllSpecs()<CR>
