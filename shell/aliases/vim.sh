7bcfc553c766dde17f4498caf097cb5f699142cf
Jonathon M. Abbott <jma@dandaraga.net>
2022-10-28 16:19:58 +1000

    nvim: Remove vim-plug

--- shell/aliases/vim.sh
+++ shell/aliases/vim.sh
@@ -1,4 +1,2 @@
 alias vim=nvim
-alias vplug='vim +PlugInstall +qall'
-alias vupdate='vim +PlugUpdate +qall'
 alias pupdate="nvim +:PackerCompile -c 'autocmd User PackerComplete quitall' -c 'PackerSync'"
