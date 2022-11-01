6853b8f33e962243d37614c1e4c04e3e4da76754
Jonathon M. Abbott <jma@dandaraga.net>
2022-11-01 11:30:01 +1000

    nvim: Configure shada for more lines per register
    Helpful when yanking large text between files across sessions.

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,11 @@
 require('init_vim')
 require('plugins')
+
+-- :help sd
+vim.opt.shada = {
+  '!', -- Save / reload global variables
+  "'100", -- Maximum number of previously edited files for which the marks are remembered
+  "<500", -- Lines saved per register
+  "s10", -- Maximum size of item content in KiB
+  "h", -- Disable hlsearch when loading shada
+}
