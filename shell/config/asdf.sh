f4ae430df362d07f255778cecc05cdf18599ffa8
Jonathon M. Abbott <jma@dandaraga.net>
2024-03-24 07:57:16 +1000

    bash: Only source asdf when installed

--- shell/config/asdf.sh
+++ shell/config/asdf.sh
@@ -1,5 +1,7 @@
-export ASDF_DATA_DIR=~/.local/share/asdf
-export ASDF_CONFIG_FILE=~/.config/asdf
+if [ -d ~/.local/share/asdf ]; then
+  export ASDF_DATA_DIR=~/.local/share/asdf
+  export ASDF_CONFIG_FILE=~/.config/asdf
 
-source "$ASDF_DATA_DIR"/asdf.sh
-source "$ASDF_DATA_DIR"/completions/asdf.bash
+  source "$ASDF_DATA_DIR"/asdf.sh
+  source "$ASDF_DATA_DIR"/completions/asdf.bash
+fi
