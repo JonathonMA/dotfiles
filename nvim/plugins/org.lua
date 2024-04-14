45fff311bd5f7ceb8e9d1ebf1e3b0d1f56aa5ed4
Jonathon M. Abbott <jma@dandaraga.net>
2024-04-14 12:17:51 +1000

    nvim: org: remove outdated setup_ts_grammar()

--- nvim/plugins/org.lua
+++ nvim/plugins/org.lua
@@ -11,7 +11,6 @@ return {
       { 'nvim-treesitter/nvim-treesitter', lazy = true },
     },
     config = function()
-      require('orgmode').setup_ts_grammar()
       require('orgmode').setup({
         org_agenda_files = {'~/Dropbox/org/*'},
         org_default_notes_file = '~/Dropbox/org/refile.org',
