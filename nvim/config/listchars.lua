74ae099e47aceda1082767d68cb90a338f6a97b1
Jonathon M. Abbott <jma@dandaraga.net>
2024-04-14 11:52:48 +1000

    nvim: Move to listchars config to lua
    Lua's table wrapper around the vim config is pretty nice here

---
+++ nvim/config/listchars.lua
@@ -0,0 +1,16 @@
+vim.opt.listchars = {
+  -- tab = '▸ ',
+  -- tab = '──▻',
+  -- tab = ' ⇥',
+  tab = '⇥ ',
+  -- eol = '¬',
+  -- eol = '⤶',
+  eol = '⮐',
+  -- space = '_',
+  space = '·',
+  -- trail = '✚',
+  trail = '×',
+  extends = '◀',
+  precedes = '▶',
+}
+vim.opt.list = false
