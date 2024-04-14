37d25509569b5f83f8dbfda3a03b62c8ee7144f7
Jonathon M. Abbott <jma@dandaraga.net>
2024-04-14 11:14:35 +1000

    nvim: Reconfigure todo-comments.nvim

--- nvim/plugins/todo-comments.lua
+++ nvim/plugins/todo-comments.lua
@@ -1,8 +1,34 @@
+-- FIX: fixme
+-- XXX: xxx
+-- WARN: warn
+-- HACK: hack
+-- TEST: test
+-- NOTE: note
+-- TODO: todo
+-- PERF: perf
+
 return {
   "folke/todo-comments.nvim",
   dependencies = { "nvim-lua/plenary.nvim" },
-  config = function()
-    require("todo-comments").setup {
-    }
-  end,
+  opts = {
+    keywords = {
+      FIX = { color = "red" },
+      WARN = { color = "orange" },
+      HACK = { color = "yellow" },
+      NOTE = { color = "blue" },
+      PERF = { color = "indigo" },
+      TODO = { icon = "󰧉", color = "magenta" },
+      TEST = { icon = "", color = "green" },
+    },
+    colors = {
+      red = { "TSVariable" },
+      orange = { "TSNumber" },
+      yellow = { "TSLabel" },
+      green = { "TSString" },
+      cyan = { "TSStringRegex" },
+      blue = { "TSFunction" },
+      indigo = { "TSKeyword" },
+      magenta = { "TSTagDelimiter" },
+    },
+  },
 }
