34ebe82a3859a944433e5702120424ac32665d36
Jonathon M. Abbott <jma@dandaraga.net>
2024-12-31 18:02:57 +1000

    sh: Switch from asdf to mise

--- shell/config/asdf.sh
+++
@@ -1,7 +0,0 @@
-if [ -d ~/.local/share/asdf ]; then
-  export ASDF_DATA_DIR=~/.local/share/asdf
-  export ASDF_CONFIG_FILE=~/.config/asdf
-
-  source "$ASDF_DATA_DIR"/asdf.sh
-  source "$ASDF_DATA_DIR"/completions/asdf.bash
-fi
