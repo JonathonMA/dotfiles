150b64ba530fcbec25d81aa8c50bba3d516d1988
Jonathon M. Abbott <jma@dandaraga.net>
2025-01-09 11:17:48 +1000

    nvim: Reconfigure nvim-tree
    - configure quit_on_open
    - bind <leader>e as well as <leader>q
    - fix lazy plugin spec

--- nvim/plugins/nvim-tree.lua
+++ nvim/plugins/nvim-tree.lua
@@ -1,18 +1,13 @@
 return {
   {
     'nvim-tree/nvim-tree.lua',
-    config = function()
-      -- disable netrw at the very start of your init.lua
-      vim.g.loaded_netrw = 1
-      vim.g.loaded_netrwPlugin = 1
-
-      -- optionally enable 24-bit colour
-      -- vim.opt.termguicolors = true
-
-      require("nvim-tree").setup()
-
-      vim.keymap.set('n', '<Leader>q', '<cmd>:NvimTreeToggle<cr>')
-    end,
+    version = "*",
+    lazy = false,
+    dependencies = { "nvim-tree/nvim-web-devicons", },
+    opts = { actions = { open_file = { quit_on_open = true, }, }, },
+    keys = {
+      { "<leader>e", "<cmd>:NvimTreeToggle<cr>", desc = "nvim-tree" },
+      { "<leader>q", "<cmd>:NvimTreeToggle<cr>", desc = "nvim-tree" },
+    },
   },
-  { 'nvim-tree/nvim-web-devicons' },
 }
