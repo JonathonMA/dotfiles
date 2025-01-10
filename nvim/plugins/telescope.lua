f60eab7e40fc2d356c51d6dfa46b32a7512b1b76
Jonathon M. Abbott <jma@dandaraga.net>
2025-01-10 19:15:52 +1000

    nvim: Rework telescope config

--- nvim/plugins/telescope.lua
+++ nvim/plugins/telescope.lua
@@ -1,10 +1,45 @@
+-- https://github.com/nvim-telescope/telescope.nvim/issues/555#issuecomment-785919858
+local makeCommandPicker = function(prompt_title, command)
+  local make_entry = require("telescope.make_entry")
+  local pickers = require("telescope.pickers")
+  local finders = require("telescope.finders")
+  local conf = require("telescope.config").values
+  return function(opts)
+    -- By creating the entry maker after the cwd options,
+    -- we ensure the maker uses the cwd options when being created.
+    opts.entry_maker = opts.entry_maker or make_entry.gen_from_file(opts)
+
+    pickers
+      .new(opts, {
+        prompt_title = prompt_title,
+        finder = finders.new_oneshot_job(
+          vim.tbl_flatten({
+            command,
+          }),
+          opts
+        ),
+        previewer = conf.file_previewer(opts),
+        sorter = conf.file_sorter(opts),
+      })
+      :find()
+  end
+end
+
 return {
-  'nvim-telescope/telescope.nvim',
-  dependencies = { 'nvim-lua/plenary.nvim' },
+  "nvim-telescope/telescope.nvim",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  keys = {
+    { "<leader>t", "<cmd>Telescope find_files<cr>", desc = "Open file" },
+    { "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Open buffer" },
+    { "<leader>fm", "<cmd>Telescope git_modified<cr>", desc = "Open modified" },
+    { "<leader>fM", "<cmd>Telescope git_branch_modified<cr>", desc = "Open modified in branch" },
+    { "<leader>f;", "<cmd>Telescope myorg<cr>", desc = "Open org" },
+    { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Live grep" },
+  },
   config = function()
-    require('telescope').setup({
+    require("telescope").setup({
       defaults = {
-        layout_strategy = 'bottom_pane',
+        layout_strategy = "bottom_pane",
         layout_config = {
           bottom_pane = {
             height = 18,
@@ -13,42 +48,9 @@ return {
       },
     })
 
-    -- https://github.com/nvim-telescope/telescope.nvim/issues/555#issuecomment-785919858
-    MakeCommandPicker = function(prompt_title, command)
-      local make_entry = require "telescope.make_entry"
-      local pickers = require "telescope.pickers"
-      local finders = require "telescope.finders"
-      local conf = require("telescope.config").values
-      return function(opts)
-        -- By creating the entry maker after the cwd options,
-        -- we ensure the maker uses the cwd options when being created.
-        opts.entry_maker = opts.entry_maker or make_entry.gen_from_file(opts)
-
-        pickers.new(opts, {
-          prompt_title = prompt_title,
-          finder = finders.new_oneshot_job(
-            vim.tbl_flatten( {
-              command
-            } ),
-            opts
-          ),
-          previewer = conf.file_previewer(opts),
-          sorter = conf.file_sorter(opts),
-        }):find()
-      end
-    end
-
-    builtin = require('telescope.builtin')
-    builtin.git_modified = MakeCommandPicker('Git Modified Files', 'modified.sh')
-    builtin.git_branch_modified = MakeCommandPicker('Git Branch Modified Files', 'branch_modified.sh')
-    builtin.myorg = MakeCommandPicker('Org', 'myorg.sh')
-
-    vim.cmd([[
-      nnoremap <leader>t <cmd>Telescope find_files<cr>
-      nnoremap <leader>l <cmd>Telescope buffers<cr>
-      nnoremap <leader>m <cmd>Telescope git_modified<cr>
-      nnoremap <leader>M <cmd>Telescope git_branch_modified<cr>
-      nnoremap <leader>; <cmd>Telescope myorg<cr>
-    ]])
+    local builtin = require("telescope.builtin")
+    builtin.git_modified = makeCommandPicker("Git Modified Files", "modified.sh")
+    builtin.git_branch_modified = makeCommandPicker("Git Branch Modified Files", "branch_modified.sh")
+    builtin.myorg = makeCommandPicker("Org", "myorg.sh")
   end,
 }
