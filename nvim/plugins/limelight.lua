7b8905146927cf5c05ddb881fad0d460a6deeaa1
Jonathon M. Abbott <jma@dandaraga.net>
2025-01-10 18:08:03 +1000

    nvim: Rebind Goyo to <leader>z

--- nvim/plugins/limelight.lua
+++ nvim/plugins/limelight.lua
@@ -1,11 +1,13 @@
 return {
   'junegunn/limelight.vim',
   dependencies = { 'junegunn/goyo.vim' },
+  keys = {
+    { "<leader>z", "<cmd>:Goyo<cr>", desc = "Zen mode" },
+  },
   config = function()
     vim.cmd [[
       autocmd! User GoyoEnter Limelight
       autocmd! User GoyoLeave Limelight!
-      map <Leader>f :Goyo<cr>
     ]]
   end,
 }
